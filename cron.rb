76b0225ae3a95e8862e775fd5122e7ff70f518d6
Kelly Martin <kellymartinv@gmail.com>
2013-06-02 12:21:46 -0500

    run as a thread so the daemon doesn't die

--- cron.rb
+++ cron.rb
@@ -4,23 +4,29 @@ require 'clockwork'
 require 'redis'
 require 'active_record'
 
-@redis_host, @redis_port = (ENV['REDIS_HOST']||'127.0.0.1:6379').split(':')
-Redis.current = Redis.new(:host => @redis_host, :port => @redis_port.to_i)
+Thread.new do
 
-database_setup = YAML.load(File.read('config/database.yml'))
-ActiveRecord::Base.establish_connection database_setup[ENV['RACK_ENV']]
+  @redis_host, @redis_port = (ENV['REDIS_HOST']||'127.0.0.1:6379').split(':')
+  Redis.current = Redis.new(:host => @redis_host, :port => @redis_port.to_i)
 
-module Clockwork
+  database_setup = YAML.load(File.read('config/database.yml'))
+  ActiveRecord::Base.establish_connection database_setup[ENV['RACK_ENV']]
 
-  every(1.day, 'rotate_bytes', at: '02:00') do
-    Redis.current.keys('bytes:*').each do |key|
-      bytes = Redis.current.get(key)
-      connector_id = key.split(':').last
-      ActiveRecord::Base.connection.execute("INSERT INTO connector_bytes (connector_id, bytes, created_at) VALUES('#{connector_id}', '#{bytes}', '#{Date.yesterday}')")
-      Redis.current.set key, 0
+  module Clockwork
+
+    every(1.day, 'rotate_bytes', at: '00:00') do
+      Redis.current.keys('bytes:*').each do |key|
+        bytes = Redis.current.get(key)
+        connector_id = key.split(':').last
+        ActiveRecord::Base.connection.execute("INSERT INTO connector_bytes (connector_id, bytes, created_at) VALUES('#{connector_id}', '#{bytes}', '#{Date.yesterday}')")
+        Redis.current.set key, 0
+      end
     end
-  end
 
+
+  end
+  Clockwork::run
 
 end
-Clockwork::run
+
+sleep
