3a436d6ae752e2b9fad5d0fa18b00f72f3584831
Kelly Martin <kellymartinv@gmail.com>
2013-06-01 21:54:31 -0500

    i guess let's make it 777 again

--- data_monitor.rb
+++ data_monitor.rb
@@ -16,10 +16,13 @@ trap('TERM') do
     :gzip => true
   })
   File.open(filename, 'a') {}
+  File.chmod(0777, filename)
   exec "kill -USR1 `cat /var/run/nginx.pid`"
 end
 
 File.open(filename, 'a') {}
+File.chmod(0777, filename)
+
 File::Tail::Logfile.open(filename) do |log|
 
   log.tail do |line|
