283ee9f3bb8317fe0ebfd2ab519800f241440118
Kelly Martin <kellymartinv@gmail.com>
2013-06-14 14:32:32 -0500

    handle the subdomain versions for pages

--- controllers/pages_controller.rb
+++ controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < SharedController
 
   get '/offline' do
     server_name = request.env['HTTP_HOST']
-    connector = Connector.where('subdomain = ? OR cname = ?', server_name, server_name).first
+    if server_name =~ /\.portly\.co$/
+      server_name.gsub!(/\.[^\.]*\.portly\.co$/,'')
+      connector = Connector.where(:subdomain => server_name).first
+    else
+      connector = Connector.where(:cname => server_name).first
+    end
     if connector
       render connector.closest_page.content, layout: :'layouts/page'
     else
