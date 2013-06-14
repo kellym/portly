559de9a5a791ef1760ce2a22d5b8444f611fe0c9
Kelly Martin <kellymartinv@gmail.com>
2013-06-14 10:31:57 -0500

    use the right variable

--- controllers/pages_controller.rb
+++ controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < SharedController
 
   get '/offline' do
-    server_name = request.env['SERVER_NAME']
+    server_name = request.env['HTTP_HOST']
     connector = Connector.where('subdomain = ? OR cname = ?', server_name, server_name).first
     if connector
       connector.closest_page.content
