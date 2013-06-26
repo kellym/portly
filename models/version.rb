d48d0c73746e1d7b7a6cca088630ce3adbab8e96
Kelly Martin <kellymartinv@gmail.com>
2013-06-25 21:12:23 -0500

    version number

--- models/version.rb
+++ models/version.rb
@@ -1,3 +1,14 @@
 class Version < ActiveRecord::Base
 
+  def number
+    return @version_number if @version_number
+    parts = self.version.split '.'
+    @version_number = 0
+    len = parts.length
+    parts.each_with_index do |num, i|
+      @version_number += num.to_i * 1000**(len-i)
+    end
+    @version_number
+  end
+
 end
