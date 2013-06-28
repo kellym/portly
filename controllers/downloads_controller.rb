f7a495ecbbdaa9b279ae524041cb718404de2cf1
Kelly Martin <kellymartinv@gmail.com>
2013-06-27 23:34:52 -0500

    download most recent from /downloads/current

--- controllers/downloads_controller.rb
+++ controllers/downloads_controller.rb
@@ -1,5 +1,10 @@
 class DownloadsController < SharedController
 
+  get '/current' do
+    v = Version.last
+    redirect "/downloads/portly-#{v.version}-#{v.number}.zip"
+  end
+
   get '/sparkle_updates.xml' do
     @versions = Version.order('created_at desc').all
     render :'downloads/sparkle_updates.xml', layout: nil
