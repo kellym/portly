f292fbf0ad2af3588a21a1da9ee040eefff04c52
Kelly Martin <kellymartinv@gmail.com>
2013-12-09 21:11:53 -0600

    port scraper for helping download assets

---
+++ models/port_scraper_service.rb
@@ -0,0 +1,55 @@
+class PortScraperService
+
+  extend Queueable
+
+  # Public: Creates a new PortScraperService model that will scrape a site with
+  # wget.
+  def initialize(connector, opts={})
+    if connector.is_a? Connector
+      @connector = connector
+    else
+      @connector = Connector.find(connector)
+    end
+    @opts = {
+      'adjust-extension'    => nil,
+      #'no-directories'      => nil,
+      'no-host-directories' => nil,
+      #'convert-links'       => nil,
+      'page-requisites'     => nil,
+      'random-wait'         => nil,
+      'timestamping'        => nil,
+      'header'              => "Accept: text/html",
+      'timeout'             => 30,
+      'reject'              => 'zip,mpg',
+      'quota'               => '20m',
+      'directory-prefix'    => "#{ROOT_PATH}/#{App.config.cache_path}#{@connector.id}"
+    }.merge opts
+    if @connector.has_authentication?
+      user = @connector.auths.first
+      if user
+        @opts['user'] = user.username
+        @opts['password'] = user.password
+      end
+    end
+  end
+
+  def perform
+    return unless @connector.mirror?
+    command = "wget"
+    @opts.each do |action, value|
+      if value
+        command += %[ --#{action}="#{value}"]
+      else
+        command +=" --#{action}"
+      end
+    end
+    command += " #{@connector.public_url}"
+    Redis.current.set("#{@connector.id}:syncing", true)
+    EventSource.publish(@connector.user_id, 'sync', id: @connector.id, state: 'started')
+    system command
+    EventSource.publish(@connector.user_id, 'sync', id: @connector.id, state: 'completed')
+  ensure
+    Redis.current.del("#{@connector.id}:syncing")
+  end
+
+end
