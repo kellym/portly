f56e56a03f05b6e741db94c6395e78f92f768ce7
Kelly Martin <kellymartinv@gmail.com>
2013-12-09 23:54:06 -0600

    that extra title ruins it

--- assets/javascripts/templates/computers._dropdown.js
+++ assets/javascripts/templates/computers._dropdown.js
@@ -30,7 +30,7 @@ function program3(depth0,data) {
   if (stack1 = helpers.ip_address) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.ip_address); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "'>\n                <span class='title'>";
+    + "'>\n                <span>";
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.name); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
